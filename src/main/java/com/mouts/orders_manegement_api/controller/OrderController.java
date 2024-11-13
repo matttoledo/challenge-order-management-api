ee87cfb8a9bda05f342e16bf2e877f53bfec76a7
toledo <maatt.toledo@gmail.com>
2024-11-13 00:46:06 -0300

    adding more configs

--- src/main/java/com/mouts/orders_manegement_api/controller/OrderController.java
+++ src/main/java/com/mouts/orders_manegement_api/controller/OrderController.java
@@ -4,11 +4,12 @@ package com.mouts.orders_manegement_api.controller;
 import com.mouts.orders_manegement_api.dto.OrderDTO;
 import com.mouts.orders_manegement_api.service.OrderManagementService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/order")
+@RequestMapping("/orders")
 @AllArgsConstructor
 public class OrderController {
 
@@ -19,9 +20,9 @@ public class OrderController {
         return ResponseEntity.ok().body(orderManagementService.getOrderById(id));
     }
 
-    @PostMapping("/create")
+    @PostMapping
     public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO orderDTO) throws Exception {
         orderManagementService.createOrder(orderDTO);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
