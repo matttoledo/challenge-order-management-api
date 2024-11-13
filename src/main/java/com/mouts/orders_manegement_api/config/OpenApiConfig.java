d8d7d8483c6cddb6f8c7af7bcf81125ced3d8fc2
toledo <maatt.toledo@gmail.com>
2024-11-12 23:47:25 -0300

    changing swagger to openApi

---
+++ src/main/java/com/mouts/orders_manegement_api/config/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.mouts.orders_manegement_api.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-api")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
