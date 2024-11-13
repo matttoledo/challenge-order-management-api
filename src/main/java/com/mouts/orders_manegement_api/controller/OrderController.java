0b34184ad1e35769bbcb0f5ff38187c984bbe5f5
toledo <maatt.toledo@gmail.com>
2024-11-12 21:13:00 -0300

    improving getmpaping

--- src/main/java/com/mouts/orders_manegement_api/controller/OrderController.java
+++ src/main/java/com/mouts/orders_manegement_api/controller/OrderController.java
@@ -2,10 +2,8 @@ package com.mouts.orders_manegement_api.controller;
 
 
 import com.mouts.orders_manegement_api.dto.OrderDTO;
-import com.mouts.orders_manegement_api.exception.OrderNotFoundException;
 import com.mouts.orders_manegement_api.service.OrderManagementService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,8 +14,8 @@ public class OrderController {
 
     private OrderManagementService orderManagementService;
 
-    @GetMapping
-    public ResponseEntity<OrderDTO> getOrderById(@RequestParam String id) throws Exception {
+    @GetMapping("/{productId}")
+    public ResponseEntity<OrderDTO> getOrderById(@PathVariable String id) throws Exception {
         return ResponseEntity.ok().body(orderManagementService.getOrderById(id));
     }
 
